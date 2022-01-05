a042942ea5b3851bcfeb74003261943d51675dd2
Marwin <80853859+Marwin-Rdgs@users.noreply.github.com>
2022-01-05 11:25:17 +0100

    Mercredi matin (amélioration carrousel + début 404)

--- src/js/fonctions.js
+++ src/js/fonctions.js
@@ -42,13 +42,14 @@ function carrousel() {
         targets: images,
         translateX: [{value: 500, duration: d},
                      {value: '-500', duration: 0, delay: function(img,ind) {
-                                    if (ind == 0) return 8*delta + 7*d;
-                                    if (ind == 1) return 7*delta + 6*d;
+                                    if (ind == 0) return 9*delta + 8*d;
+                                    if (ind == 1) return 8*delta + 7*d;
+                                    if (ind == 2) return 7*delta + 6*d;
                                     if (ind == 3) return 6*delta + 5*d;
-                                    if (ind == 4) return 5*delta+4*d;
-                                    if (ind == 5) return 4*delta+3*d;
+                                    if (ind == 4) return 5*delta + 4*d;
+                                    if (ind == 5) return 4*delta + 3*d;
                                     if (ind == 6) return 3*delta + 2*d;
-                                    if (ind == 7) return 2*delta+d;
+                                    if (ind == 7) return 2*delta + d;
                                     if (ind == 8) return delta;
                                     return 0;
                                 }},
@@ -69,38 +70,5 @@ function carrousel() {
             if (ind == 8) return 9*delta + 8*d;
             return 10*delta + 9*d;
         }
-    });
-}
-
-function carrouselAlternate() {
-    let images = document.querySelectorAll('.image15');
-    let d = 2000;
-    let delta = 1000;
-
-    images.forEach(function(img,ind) {
-        img.style.zIndex = images.length - ind;
-    });
-
-    let anim15 = anime.timeline({
-        targets: '.imgalt',
-        loop: true,
-        direction: 'alternate',
-        easing: 'linear',
-        delay: function(img,ind) {
-            if (ind == 0) return delta;
-            if (ind == 1) return 2*delta + d;
-            if (ind == 2) return 3*delta + 2*d;
-            if (ind == 3) return 4*delta + 3*d;
-            if (ind == 4) return 5*delta + 4*d;
-            if (ind == 5) return 6*delta + 5*d;
-            if (ind == 6) return 7*delta + 6*d;
-            if (ind == 7) return 8*delta + 7*d;
-            if (ind == 8) return 9*delta + 8*d;
-            return 9*delta + 8*d;
-        }
-    });
-    anim15.add({
-        translateX: [{value: 500, duration: d, easing: 'easeInOutCubic'},
-                     {value: 500, duration: 0, delay: delta}]
     });
 }
